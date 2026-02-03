d03a52798176b3a7c4846fb970b9db54d5b3300a
Daniel Leite de Barros Teixeira <06113721183@seguranca.local>
2026-02-03 10:45:06 -0400

    chore: initial Spring Boot project structure

---
+++ artists-api/src/main/java/br/gov/seplag/artists_api/ArtistsApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.seplag.artists_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistsApiApplication.class, args);
+	}
+
+}
