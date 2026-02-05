e826bc925f8bba37e1ebb1588736ac23d1a38e9d
danielleit <daniellbt13@gmail.com>
2026-02-04 23:02:05 -0300

    chore: add temporary security config without JWT

---
+++ artists-api/src/main/java/br/gov/seplag/artists_api/common/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package br.gov.seplag.artists_api.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+/**
+ * Temporary security configuration.
+ *
+ * All requests are permitted while JWT authentication
+ * is under development.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
